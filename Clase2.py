bacd5110fb6743890577b7198bffbcccbcbe8050
FedeOjeda <ojedaf45@gmail.com>
2020-09-01 15:34:09 -0300

    Teoría Primera Parte

---
+++ Clase2.py
@@ -0,0 +1,25 @@
+# List Comprehension
+# Listas avanzadas en 1 linea de código:
+# lista=[ expresion for elemento in iterable]
+
+# Método Tradicional
+lista = []
+for numero in range (0,11):
+    lista.append(numero**2)
+print (lista)
+
+# Con conprensión de Listas
+lista = [numero**2 for numero in range(0,11)]
+print (lsita)
+<<<<<<< HEAD
+
+
+# Lambda
+# Funciones Anónimas
+def doblar(num):
+    resultado = num * 2
+    return resultado
+
+doblar(2)    
+=======
+>>>>>>> 8c59dac0f8b33429dad6f0cbcfa45e62eae2536b
